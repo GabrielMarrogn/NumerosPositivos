d24bc3392e5734baae55494e56ad47531df37ffc
GabrielMarrogn <gabrielosantos2012@hotmail.com>
2024-01-07 01:21:22 -0300

    Second

--- src/App.java
+++ src/App.java
@@ -4,14 +4,19 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         int contador = 0;
+        double soma = 0;
         for(int i = 0; i < 6; i++){
             double a = sc.nextDouble();
             if(a >0 ){
                 contador++;
+                soma = soma + a;
+
             }
         }
 
         sc.close();
+        soma = soma/contador;
         System.out.println(contador +" valores positivos");
+        System.out.printf("%.1f\n", soma);
     }
 }
